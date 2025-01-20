a8dd994c6413ae6bc39d02e0b39bd40486f9f688
quangha-dev <quangha.dev@gmail.com>
2025-01-20 18:42:35 +0700

    Add files via upload

---
+++ bai21_Nguyen_Quang_Ha/bai21/Controllers/CircleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bai21.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleController : ControllerBase
+    {
+
+        [HttpGet("cv_dt")]
+        public IActionResult TinhChuViDienTichDuongKinh(double rr) // rr là bán kính
+        {
+            if (rr <= 0)
+            {
+                return BadRequest("Bán kính phải dương");
+            }
+
+            double cv = 3.14 * 2 * rr;  // Chu vi
+            double dt = 3.14 * rr * rr; // Diện tích
+            double dk = 2 * rr;         // Đường kính
+
+            var jsonStr = new
+            {
+                dien_tich = dt,
+                chu_vi = cv,
+                duong_kinh = dk
+            };
+
+            return Ok(jsonStr);
+        }
+    }
+}
---
+++ bai21_Nguyen_Quang_Ha/bai21/Program.cs
@@ -0,0 +1,34 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+        c.RoutePrefix = "swagger"; // Đặt Swagger UI ở /swagger
+    });
+}
+
+app.UseHttpsRedirection();
+
+// Điều hướng mặc định tới bai21.html khi truy cập "/"
+app.MapGet("/", async context =>
+{
+    await context.Response.SendFileAsync("bai21.html");
+});
+
+// Middleware xử lý API
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
